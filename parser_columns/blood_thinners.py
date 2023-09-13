4f59ec2b3c7157c84d3bca8e2d084bf5f38cc1b6
bruria <bruriah1999@gmail.com>
2023-09-13 19:59:24 +0300

    cr modifications, before TODO

--- parser_columns/blood_thinners.py
+++ parser_columns/blood_thinners.py
@@ -1,18 +1,17 @@
 from constants import MISSING_VALUE_STRING
 from parser_columns.base_parser import BaseParser
 
-CONTENT = 'Content'
 # TODO two typos found קסלטו, אפסירין
-BLOOD_THINNERS_TYPES = ['אספירין', 'מיקרופירין', 'קרטיה', 'פלביקס', 'קלופידרוגל', 'קסרלטו', 'אליקוויס', 'קומדין',
+BLOOD_THINNERS_KEYWORD_TYPES = ['אספירין', 'מיקרופירין', 'קרטיה', 'פלביקס', 'קלופידרוגל', 'קסרלטו', 'אליקוויס', 'קומדין',
                         'קלקסל']
-BLOOD_THINNERS = 'מדללי דם'
+BLOOD_THINNERS_KEYWORD = 'מדללי דם'
 
 
 class BloodThinners(BaseParser):
     def parse_document(self, document_content: str) -> str:
-        for blood_thinner in BLOOD_THINNERS_TYPES:
+        for blood_thinner in BLOOD_THINNERS_KEYWORD_TYPES:
             if blood_thinner in document_content:
                 return blood_thinner
-        if BLOOD_THINNERS in document_content:
-            return BLOOD_THINNERS
+        if BLOOD_THINNERS_KEYWORD in document_content:
+            return BLOOD_THINNERS_KEYWORD
         return MISSING_VALUE_STRING
