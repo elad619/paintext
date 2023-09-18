07ee13513cbfd4bc98d1bf589d1cce75f7afa28b
SherlockSec <dan@lights.htb>
2023-09-18 23:57:42 +0300

    use parse function instead of implementing it, cleaner code

--- parser_columns/date_of_operation_parser.py
+++ parser_columns/date_of_operation_parser.py
@@ -1,37 +1,24 @@
+from typing import List
+import datetime as dt
 from parser_columns.base_parser import BaseParser
 from constants import MISSING_VALUE_STRING
-import pandas as pd
-from datetime import datetime
 import re
+from dateutil.parser import parse as date_parser, ParserError
 
 DATE_STRING_REGEX_FORMAT = r'\d+[\./]\d+[\./]\d+'
-DATE_FORMATS = ["%d/%m/%y", "%d.%m.%y"]
 
 
 class OperationDateParser(BaseParser):
     def __init__(self):
         self.date_string_format = DATE_STRING_REGEX_FORMAT
-        self.possible_date_formats = DATE_FORMATS
-
-    def _convert_string_to_date(self, date_as_string: str) -> datetime:
-        for date_format in self.possible_date_formats:
-            try:
-                date_obj = datetime.strptime(date_as_string, date_format)
-                return date_obj
-            except ValueError:
-                continue
-        raise ValueError(f"Cannot convert {date_as_string} to date!")
+        self.dates: List[dt.date] = []
 
     def parse_document(self, document_content: str) -> str:
-        all_dates_in_documents = re.findall(self.date_string_format, document_content)
-        if len(all_dates_in_documents) != 0:
-            try:
-                all_dates_in_documents_as_datetime = [self._convert_string_to_date(date_in_document) for
-                                                      date_in_document in
-                                                      all_dates_in_documents]
-                most_recent_date_in_document = max(all_dates_in_documents_as_datetime).date()
-                return str(most_recent_date_in_document)
-            except:
-                return MISSING_VALUE_STRING
+        dates_in_documents: List[str] = re.findall(self.date_string_format, document_content)
 
-        return MISSING_VALUE_STRING
+        for date in dates_in_documents:
+            try:
+                self.dates.append(date_parser(date).date())
+            except ParserError as e:
+                print(e)
+        return str(max(dates_in_documents)) if dates_in_documents else MISSING_VALUE_STRING
