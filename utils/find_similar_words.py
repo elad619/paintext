3c0571ab36bdcf35196c0369aa30ca0b5abd7cb1
Guy-Shapira <guy.shapira@campus.technion.ac.il>
2023-09-18 20:18:17 +0300

    refactor: complete refactor of util file

--- utils/find_similar_words.py
+++ utils/find_similar_words.py
@@ -1,14 +1,20 @@
-from fuzzywuzzy import process, fuzz
 from typing import List
 
-def find_similar_words(text: str, word: str) -> List[str]:
-    words = text.split()
-    n = len(words)
-    similar_words = []
+from fuzzywuzzy import fuzz
 
-    for w in words:
-        similarity = fuzz.ratio(w, word)
-        if similarity >= (n - 1) / n * 100:
+
+def find_similar_words(text: str, word: str) -> List[str]:
+    words_in_text = text.split()
+    length_of_word = len(word)
+    similar_words = []
+    minimum_similarity_ratio_threshold = (length_of_word - 1) / length_of_word * 100
+    for w in words_in_text:
+        if _compute_similarity_ratio_and_compare_to_threshold(minimum_similarity_ratio_threshold, w, word):
             similar_words.append(w)
 
     return similar_words
+
+
+def _compute_similarity_ratio_and_compare_to_threshold(threshold: float, s1: str, s2: str) -> bool:
+    similarity_ratio_of_strings = fuzz.ratio(s1, s2)
+    return similarity_ratio_of_strings >= threshold
