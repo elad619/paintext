dac4afb71d0f13318256337bbc1ead2148a4c011
Guy-Shapira <guy.shapira@campus.technion.ac.il>
2023-09-18 20:09:01 +0300

    changed naming convention for constant strings

--- parser_columns/contains_marcaine_medicine.py
+++ parser_columns/contains_marcaine_medicine.py
@@ -1,20 +1,20 @@
 from parser_columns.base_parser import BaseParser
 from utils.find_similar_words import find_similar_words
-from constants import POSITIVE_VALUE_STRING, NEGATIVE_VALUE_STRING, MISSING_VALUE_STRING
+from constants import POSITIVE_VALUE, NEGATIVE_VALUE, MISSING_VALUE
 
-MARCAINE_STRING = "מרקאין"
+MARCAINE = "מרקאין"
 
 class DoesContainMarcaineMedicineParser(BaseParser):
     def __init__(self) -> None:
         super().__init__()
-        self.marcaine_string = MARCAINE_STRING
+        self.marcaine_string = MARCAINE
 
     def parse_document(self, document_content: str) -> str:
         try:
             similar_words_in_text = find_similar_words(document_content, self.marcaine_string)
             if len(similar_words_in_text) > 0:
-                return POSITIVE_VALUE_STRING
+                return POSITIVE_VALUE
             else:
-                return NEGATIVE_VALUE_STRING
+                return NEGATIVE_VALUE
         except:
-            return MISSING_VALUE_STRING
+            return MISSING_VALUE
