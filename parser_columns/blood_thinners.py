2a9fa767b114d789b9e9f65da9c6c439fb5f01ab
bruria <bruriah1999@gmail.com>
2023-09-26 13:32:33 +0300

    OptionBAsedParser blood thinners parser

--- parser_columns/blood_thinners.py
+++ parser_columns/blood_thinners.py
@@ -1,28 +1,9 @@
-from typing import List
+from option_based_parasers.base_option_from_group_parser import OptionBasedParser
 
-from utils.word_searcher import WordSearcher
-
-from base_parser import BaseParser
-from constants import MISSING_VALUE_STRING
-
-
-class OptionBasedParser(BaseParser):
-    def __init__(self, options: List[str] = None):
-        self.word_searchers_list = [WordSearcher(word=option) for option in options]
-
-    def _get_matches_for_all_options(self, document_content: str) -> List[str]:
-        options_found_in_text = []
-        for word_searcher in self.word_searchers_list:
-            if (word_searcher.word in document_content) or (
-                    len(word_searcher.find_similar_words(document_content)) > 0):
-                options_found_in_text.append(word_searcher.word)
-        return sorted(options_found_in_text)
-
+from constants import MISSING_VALUE
 
 BLOOD_THINNERS_KEYWORD_TYPES = ['אספירין', 'מיקרופירין', 'קרטיה', 'פלביקס', 'קלופידרוגל', 'קסרלטו', 'אליקוויס',
                                 'קומדין', 'קלקסל']
-BLOOD_THINNERS_KEYWORD = 'מדללי דם'
-DOESNT_TAKE_BLOOD_THINNERS_MESSAGE = 'ללא'
 
 
 class BloodThinners(OptionBasedParser):
@@ -32,6 +13,6 @@ class BloodThinners(OptionBasedParser):
     def parse_document(self, document_content: str) -> str:
         options_found_in_document = self._get_matches_for_all_options(document_content)
         if len(options_found_in_document) == 0:
-            options_found_in_document.append(MISSING_VALUE_STRING)
+            options_found_in_document.append(MISSING_VALUE)
         sorted_options_found_in_document = sorted(options_found_in_document)
         return ",".join(sorted_options_found_in_document)
