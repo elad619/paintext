b6840c1567ef9278324b6f17e803737068f68f9d
bruria <bruriah1999@gmail.com>
2023-09-26 07:46:45 +0300

    blood thinners using OptionBasedParser

--- parser_columns/blood_thinners.py
+++ parser_columns/blood_thinners.py
@@ -1,21 +1,37 @@
-from utils.find_similar_words import find_similar_words
+from typing import List
 
+from utils.word_searcher import WordSearcher
+
+from base_parser import BaseParser
 from constants import MISSING_VALUE_STRING
-from parser_columns.base_parser import BaseParser
+
+
+class OptionBasedParser(BaseParser):
+    def __init__(self, options: List[str] = None):
+        self.word_searchers_list = [WordSearcher(word=option) for option in options]
+
+    def _get_matches_for_all_options(self, document_content: str) -> List[str]:
+        options_found_in_text = []
+        for word_searcher in self.word_searchers_list:
+            if (word_searcher.word in document_content) or (
+                    len(word_searcher.find_similar_words(document_content)) > 0):
+                options_found_in_text.append(word_searcher.word)
+        return sorted(options_found_in_text)
+
 
 BLOOD_THINNERS_KEYWORD_TYPES = ['אספירין', 'מיקרופירין', 'קרטיה', 'פלביקס', 'קלופידרוגל', 'קסרלטו', 'אליקוויס',
-                                'קומדין',
-                                'קלקסל']
+                                'קומדין', 'קלקסל']
 BLOOD_THINNERS_KEYWORD = 'מדללי דם'
 DOESNT_TAKE_BLOOD_THINNERS_MESSAGE = 'ללא'
 
 
-class BloodThinners(BaseParser):
+class BloodThinners(OptionBasedParser):
+    def __init__(self):
+        super().__init__(options=BLOOD_THINNERS_KEYWORD_TYPES)
+
     def parse_document(self, document_content: str) -> str:
-        for blood_thinner in BLOOD_THINNERS_KEYWORD_TYPES:
-            if (blood_thinner in document_content) or (len(find_similar_words(document_content, blood_thinner)) > 0):
-                return blood_thinner
-        if (BLOOD_THINNERS_KEYWORD in document_content) or (
-                len(find_similar_words(document_content, BLOOD_THINNERS_KEYWORD)) > 0):
-            return DOESNT_TAKE_BLOOD_THINNERS_MESSAGE
-        return MISSING_VALUE_STRING
+        options_found_in_document = self._get_matches_for_all_options(document_content)
+        if len(options_found_in_document) == 0:
+            options_found_in_document.append(MISSING_VALUE_STRING)
+        sorted_options_found_in_document = sorted(options_found_in_document)
+        return ",".join(sorted_options_found_in_document)
--- parser_columns/date_of_operation_parser.py
+++ parser_columns/date_of_operation_parser.py
@@ -1,23 +1,37 @@
-from typing import List
-import datetime as dt
 from parser_columns.base_parser import BaseParser
-from constants import MISSING_VALUE
+from constants import MISSING_VALUE_STRING
+import pandas as pd
+from datetime import datetime
 import re
-from dateutil.parser import parse as date_parser, ParserError
 
 DATE_STRING_REGEX_FORMAT = r'\d+[\./]\d+[\./]\d+'
+DATE_FORMATS = ["%d/%m/%y", "%d.%m.%y"]
 
 
 class OperationDateParser(BaseParser):
     def __init__(self):
         self.date_string_format = DATE_STRING_REGEX_FORMAT
-        self.dates: List[dt.date] = []
+        self.possible_date_formats = DATE_FORMATS
+
+    def _convert_string_to_date(self, date_as_string: str) -> datetime:
+        for date_format in self.possible_date_formats:
+            try:
+                date_obj = datetime.strptime(date_as_string, date_format)
+                return date_obj
+            except ValueError:
+                continue
+        raise ValueError(f"Cannot convert {date_as_string} to date!")
 
     def parse_document(self, document_content: str) -> str:
-        dates_in_documents: List[str] = re.findall(self.date_string_format, document_content)
-        for date in dates_in_documents:
+        all_dates_in_documents = re.findall(self.date_string_format, document_content)
+        if len(all_dates_in_documents) != 0:
             try:
-                self.dates.append(date_parser(date.replace(".", "/")).date())
-            except ParserError as e:
-                print(e)
-        return str(max(dates_in_documents)) if dates_in_documents else MISSING_VALUE
+                all_dates_in_documents_as_datetime = [self._convert_string_to_date(date_in_document) for
+                                                      date_in_document in
+                                                      all_dates_in_documents]
+                most_recent_date_in_document = max(all_dates_in_documents_as_datetime).date()
+                return str(most_recent_date_in_document)
+            except:
+                return MISSING_VALUE_STRING
+
+        return MISSING_VALUE_STRING
